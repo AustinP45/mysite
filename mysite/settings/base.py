a55ad91a7675f73d7aeb522c0f3e06b0c8a87d6d
Austin Peterson <austinpeterson@outlook.com>
2015-02-09 23:05:45 -0600

    Updated wsgi

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -82,6 +82,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# FROM HEROKU #
+###############
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
