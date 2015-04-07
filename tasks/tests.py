3dde1c94df4eb71f834db72c9e7ee26d19de4f17
Austin <austinpeterson@outlook.com>
2015-04-06 23:54:12 -0500

    Update tests.py

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+class QuestionViewTests(TestCase):
+    def test_index_view_with_no_questions(self):
+		  """
+        If no tasks exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('tasks:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No tasks are available.")
+        self.assertQuerysetEqual(response.context['todo_list'], [])
